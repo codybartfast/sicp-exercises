23575aa5a1ff8c18e234ef10c82e05ce852b05b6
John Donnellan <john@blueTribu.com>
2016-03-11 09:37:30 +0000

    add potential document structure

--- SicpBook/src/XmlFromHtml.fsx
+++ SicpBook/src/XmlFromHtml.fsx
@@ -13,8 +13,8 @@ let fileSortKey file =
     |> int
 
 let htmlFiles = 
-    Directory.GetFiles(htmlDir, "*.html") 
-    |> Array.sortBy fileSortKey
+    (new DirectoryInfo(htmlDir)).GetFiles("*.html")
+    |> Array.sortBy (fun file -> fileSortKey file.FullName)
 
 let xmlEscapeRx = new Regex("""[&<>'"]""", RegexOptions.Compiled)
 let xmlEscape text = 
@@ -26,13 +26,32 @@ let xmlEscape text =
         | "'" -> "&apos;"
         | "\"" -> "&quot;"
         | unexpected -> failwith "got unexpected: " + unexpected))
-    
 
-let content = 
-    Array.head htmlFiles
-    |> File.ReadAllText
-    |> xmlEscape
+type File = { Id: Id; Content: Content }
+and Content = 
+    | Bookend of Html
+    | Chapter of Chapter
+    | Section of Section
 
+and Chapter = { Id: Id; Title: Title; Epigraph: Epigraph; Prose: Prose; Html: Html}
+and Section = { Id: Id; Title: Title; Prose: Prose; Subsections: Subsection list; Html: Html }
+and Subsection = { Id: Id; Title: Title; Text: Text list; Html: Html }
+and Epigraph = Epigraph of Html
+and Text = Prose | Exercise
+and Prose = { Html: Html }
+and Exercise = { Id: Id; Html: Html }
+and Html = Html of string
+and Id = Id of string
+and Title = Title of string
 
+let parseText text = text |> Html |> Bookend
 
+let parseFile (file : FileInfo) = {
+    File.Id = Id file.Name;
+    Content =  (parseText (File.ReadAllText(file.FullName)));
+    }
 
+let files = 
+    htmlFiles
+    |> Array.map parseFile
+    |> Array.head
