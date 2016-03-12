9338dd7a50c6e7f0cef3d4771a95448272ff3705
John Donnellan <john@blueTribu.com>
2016-03-12 13:13:35 +0000

    add first Epigraph

--- SicpBook/src/Regex.fsx
+++ SicpBook/src/Regex.fsx
@@ -1,7 +1,18 @@
 ﻿
 open System.Text.RegularExpressions
 
+let chapterRx = new Regex("""<a name="%_chap_\d">""", RegexOptions.Compiled)    
 let (|ChapterRx|_|) text =
-    let m = Regex.Match(text, ">Chapter \d<")
-    if m.Success then Some text else None
+    if chapterRx.IsMatch(text) then Some text else None
+ 
+let sectionRx = new Regex("""<a name="%_sec_\d\.\d+"></a>""", RegexOptions.Compiled)    
+let (|SectionRx|_|) text =
+    if sectionRx.IsMatch(text) then Some text else None
+
+//let epigraphRx = new Regex("""(?<=\<span class=epigraph>).*?(?=</span>)""", RegexOptions.Compiled ||| RegexOptions.Singleline)
+let epigraphRx = new Regex("""<div align=right>.*?</div>""", RegexOptions.Compiled ||| RegexOptions.Singleline)
+let (|EpigraphRx|_|) text =
+    let m = epigraphRx.Match(text)
+    if m.Success then Some m.Value else None
+    
     
--- SicpBook/src/Tree.fsx
+++ SicpBook/src/Tree.fsx
@@ -4,6 +4,8 @@
 open System.IO
 open System.Text.RegularExpressions
 
+open Regex
+
 let htmlDir = Common.htmlDir
 printfn "Got Html Directory: %s" htmlDir
 
@@ -30,12 +32,13 @@ let xmlEscape text =
 
 type File = { Id: Id; Content: FileContent }
 and FileContent = 
-    | Bookend of Html
     | Chapter of Chapter
     | Section of Section
+    | Bookend of Bookend
 
 and Chapter = { Id: Id; Title: Title; Epigraph: Epigraph; Prose: Prose; Html: Html}
 and Section = { Id: Id; Title: Title; Prose: Prose; Subsections: Subsection list; Html: Html }
+and Bookend = {Epigraph : Epigraph option; Prose: Prose}
 and Subsection = { Id: Id; Title: Title; Text: Text list; Html: Html }
 and Epigraph = Epigraph of Html
 and Text = Prose | Exercise
@@ -45,20 +48,42 @@ and Html = Html of string
 and Id = Id of string
 and Title = Title of string
 
+let epigraph text =
+    match text with
+    | EpigraphRx t -> Some (Epigraph (Html t))
+    | _ -> None
+
+let bookend text = 
+    Bookend {Epigraph = epigraph text; Prose = {Html = Html text}}
+
 let parseFile (file : FileInfo) = 
     let text = File.ReadAllText(file.FullName);
     {    
         File.Id = Id file.Name;
         Content =      
             match text with
-            | ChapterRx t -> Chapter {Chapter.Id = Id "id"; Title = Title "title"; Epigraph = Epigraph (Html "epigraph"); Prose = {Html = Html "prose"}; Html = Html text}
-            | _ -> Bookend (Html text)
+            | ChapterRx t -> Chapter {Id = Id "id"; Title = Title "title"; Epigraph = Epigraph (Html "epigraph"); Prose = {Html = Html "prose"}; Html = Html text}
+            | SectionRx t -> Section {Id = Id "id"; Title = Title "title"; Prose = {Html = Html "prose"}; Subsections = []; Html = Html text}
+            | _ -> bookend text
     }
 
-let files () = 
-    htmlFiles
-    |> Array.map parseFile
-    |> Array.iter (fun f ->
-        printfn "%A" (f.Content.GetType().Name))
+let files =  htmlFiles |> Array.map parseFile
 
+let go () =
+    files 
+        |> Array.iter (fun f ->        
+            printfn "%A" (f.Content.GetType().Name))
+    printfn "%i" files.Length
 
+let six = 
+    match files.[6].Content with
+    | Bookend b -> b
+    | _ -> failwith "hello there"
+
+let e = six.Epigraph
+
+let h = six.Prose.Html
+
+let stringFromHtml = function Html s -> s
+
+let s = stringFromHtml h
