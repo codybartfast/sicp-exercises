ba6546c409bdacd65267d11641501cf4ee72d948
John Donnellan <john@blueTribu.com>
2016-03-15 14:08:42 +0000

    update to put images and stylesheet in separate directories

--- SicpBook/src/Common.fsx
+++ SicpBook/src/Common.fsx
@@ -1,8 +1,25 @@
 ﻿open System.IO
 
-let htmlDir = 
-    (new FileInfo(__SOURCE_DIRECTORY__ + @"\..\book-html")).FullName
+let getDirectory parentPath childName =
+    let path = Path.Combine(parentPath, childName)
+    let dir = new DirectoryInfo(path)
+    dir.Create()
+    dir.FullName
 
-let xmlDir = 
-    (new FileInfo(__SOURCE_DIRECTORY__ + @"\..\book-xml")).FullName
+let htmlRoot = 
+    let relativePath = __SOURCE_DIRECTORY__ + @"\..\html"
+    let rootPath = (new DirectoryInfo(relativePath)).FullName
+    getDirectory rootPath ""
 
+let htmlImages = getDirectory htmlRoot "images"
+
+let htmlStylesheets = getDirectory htmlRoot "stylesheets"
+
+//let xmlDir = 
+//    (new FileInfo(__SOURCE_DIRECTORY__ + @"\..\book-xml")).FullName
+
+
+
+//let rootDir = getDirectory ""
+//let imagesDir = getDirectory "images"
+//let stylesheetDir = getDirectory "stylesheets"
--- SicpBook/src/PullBook.fsx
+++ SicpBook/src/PullBook.fsx
@@ -9,68 +9,85 @@ open System.Threading.Tasks
 open System.Collections.Generic
 
 # load ".\Common.fsx"
-
+open Common
+    
 let toText = Encoding.ASCII.GetString
 
 let print text =     
     printfn "%s" text
     text
+
+let client = new HttpClient()
+let download baseUrl file=
+    client.GetByteArrayAsync(baseUrl + file : string).Result    
+
+let saveText saveDir file text =
+    File.WriteAllText(Path.Combine(saveDir, file), text)
+    text
+
+let downloadAndSave baseUrl saveDir file =
+    let save saveDir file bytes =
+        File.WriteAllBytes(Path.Combine(saveDir, file), bytes)
+        bytes
+    file 
+    |> download baseUrl
+    |> save saveDir file
     
 let splitUrl url =
-    // "https://mitpress.mit.edu/sicp/full-text/book/book.html"
-    //   -> "https://mitpress.mit.edu/sicp/full-text/book/", "book.html" 
+    // e.g.: "https://mitpress.mit.edu/sicp/full-text/book/book.html"
+    //          -> "https://mitpress.mit.edu/sicp/full-text/book/"
+    //              & "book.html" 
     match Regex.Split(url, @"(?<=/)(?=[^/]+$)") with
     | [| baseUrl; filename |] -> baseUrl, filename
     | _ -> failwith ("failed to split url: " + url)
 
-let client = new HttpClient()
-let downloadAndSave baseUrl outDir file =
-    let download url =
-        client.GetByteArrayAsync(url:string).Result    
-    let save outDir file bytes =
-        File.WriteAllBytes(Path.Combine(outDir, file), bytes)
-        bytes
-    baseUrl + file 
-    |> download
-    |> save outDir file
+
+let updateReference html =
+    // Matches the filename in an image or stylesheet reference.
+    //                      follows              match       
+    //                    <--------->  <---------------------> 
+    let pattern = """(?<=(href|src)=") [-\w]+\.(gif|jpg|(css)) """
+                                                          
+    let replace (m : Match) = 
+        let dirname =               
+            if m.Groups.[3].Success // the parenthesis around (css)
+            then "stylesheets"
+            else "images" 
+        dirname + "/" + m.Value
+
+    Regex.Replace(html, pattern, replace, RegexOptions.IgnorePatternWhitespace)
 
 let downloadedImages = new HashSet<string>()
-let pullImages baseUrl outDir text =
-    // 'img' group mataches text like "cover.jpg"
+let pullImages baseUrl text =
+    // 'imgage' group mataches text like "cover.jpg"
     let image = """<img src="(?<imgage>[^"]+)"""
     Regex.Matches(text, image)
     |> Seq.cast
     |> Seq.map (fun (m : Match )-> m.Groups.["imgage"].Value)
     |> Seq.filter downloadedImages.Add
-    |> Seq.iter (downloadAndSave baseUrl outDir >> ignore)
+    |> Seq.iter (downloadAndSave baseUrl htmlImages >> ignore)
     text
 
-let rec pullPage baseUrl outDir file =
+let rec pullPage baseUrl file =
     file
-    |> downloadAndSave baseUrl outDir 
+    |> download baseUrl
     |> toText
+    |> pullImages baseUrl 
+    |> updateReference
+    |> saveText htmlRoot file
     |> print
-    |> pullImages baseUrl outDir
-    |> pullNext baseUrl outDir
+    |> pullNext baseUrl
 
-and pullNext baseUrl outDir text =
+and pullNext baseUrl text =
     // 'next' group mataches text like "book-Z-H-1.html"
     let next = """href="(?<next>[^"]+)">next"""
     let m = Regex.Match(text, next)
     match m.Success with
     | false -> "No Next!"
-    | true -> pullPage baseUrl outDir (m.Groups.["next"].Value)  
+    | true -> pullPage baseUrl (m.Groups.["next"].Value)  
 
-//[<EntryPoint>]
-let main argv = 
-    let page1 = @"https://mitpress.mit.edu/sicp/full-text/book/book.html"
-    let outDir = Common.htmlDir
-    Directory.CreateDirectory(outDir) |> ignore
+let page1 = @"https://mitpress.mit.edu/sicp/full-text/book/book.html"    
+let baseUrl, filename = splitUrl page1
 
-    let baseUrl, filename = splitUrl page1
-    pullPage baseUrl outDir filename |> printfn "%s"
-    downloadAndSave baseUrl outDir "book-Z-C.css" |> ignore
-    Console.ReadKey() |> ignore
-    0
-
-main [||]
+pullPage baseUrl filename |> printfn "%s"
+downloadAndSave baseUrl htmlStylesheets "book-Z-C.css" |> ignore
